0720e4a3a011a2e5ae6d2ceef293738546b212a1
batalygin <batalygin@reksoft.ru>
2018-04-24 21:00:04 +0300

    Added lesson 1 homework

--- Lesson1/src/main/java/ru/reksoft/Lesson1.java
+++ Lesson1/src/main/java/ru/reksoft/Lesson1.java
@@ -2,7 +2,7 @@ package ru.reksoft;
 
 public class Lesson1 {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args){
+        System.out.println("Hello, Alexander!\nLesson 1 Homework");
     }
 }
